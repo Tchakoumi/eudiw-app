ff55565e468413fcfd31f11fda6148c0a9fe27dc
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-08 12:53:01 +0100

    feat(app): #2 installed next-pwa and configured

--- apps/wallet/next.config.js
+++ apps/wallet/next.config.js
@@ -2,17 +2,24 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: false,
+  register: true,
+  scope: '/',
+  sw: 'service-worker.js',
+});
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig = withPWA({
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
+});
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
