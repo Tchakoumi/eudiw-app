c29942e5c239d207b1053e263be32a98aa66423f
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-09 17:07:29 +0100

    chore(app): #5 picked connection_string from router query before sending

--- apps/wallet/pages/scan-details/index.tsx
+++ apps/wallet/pages/scan-details/index.tsx
@@ -1,14 +1,17 @@
 import { Box } from '@mui/material';
 import ConnectionDialog from '../../components/scanDetails/connectionDialog';
+import { useRouter } from 'next/router';
 
 export default function Index() {
+  const {
+    query: { connection_string },
+  } = useRouter();
   return (
     <Box>
       <ConnectionDialog
-        connectionLink="https://google.com"
+        connectionLink={(connection_string as string) ?? ''}
         isDialogOpen={true}
       />
-      Scan details page
     </Box>
   );
 }
