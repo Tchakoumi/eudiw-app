afe0cf2a7e9fcdd39a0332bb81e9a5992b47e30a
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-16 15:22:44 +0100

    feat(app): #1 moved app component to pages folder

--- apps/wallet-react/src/main.tsx
+++ apps/wallet-react/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
-import App from './app/app';
+import App from './pages/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
