6f0e3236cf2ad2c63308c70e6fdb5a66d3b6c935
Ingrid Kamga <ingrid.kamga@clemios.com>
2024-01-16 12:23:02 +0100

    test(shared): suggest basic test for usePWA lib

--- utils/usePWA/src/lib/usePWA.spec.tsx
+++ utils/usePWA/src/lib/usePWA.spec.tsx
@@ -1,10 +1,19 @@
+import { usePWA, InstallPWAContextProvider } from '../index';
 import { render } from '@testing-library/react';
 
-import UsePWA from './usePWA';
+describe('usePWA', () => {
+  it('should be used successfully', () => {
+    const UsingPWA = () => {
+      const { isInstallable } = usePWA();
+      return <div>{isInstallable}</div>;
+    };
 
-describe('UsePWA', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<UsePWA />);
+    const { baseElement } = render(
+      <InstallPWAContextProvider>
+        <UsingPWA></UsingPWA>
+      </InstallPWAContextProvider>
+    );
+
     expect(baseElement).toBeTruthy();
   });
 });
