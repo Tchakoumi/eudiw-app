30c2819707cfc1b7ee6826cd2e51060f1f7bb641
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-17 09:15:27 +0100

    feat(app): #1 added scan route to routing in react-based wallet app

--- apps/wallet-react/src/routes.tsx
+++ apps/wallet-react/src/routes.tsx
@@ -1,4 +1,6 @@
+import { Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Scan from './pages/scan/Scan';
 
 export interface IRoute {
   path: string;
@@ -10,5 +12,13 @@ export const routes: IRoute[] = [
   {
     element: <Home />,
     path: '/',
+  },
+  {
+    element: <Scan />,
+    path: '/scan',
+  },
+  {
+    element: <Navigate to="/" />,
+    path: '*',
   },
 ];
