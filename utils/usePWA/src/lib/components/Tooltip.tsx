459ce74f5e07376b0c8d43e28ebc66b465cad16e
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-01 16:06:59 +0100

    feat(shared): #2 built default tooltip directive component

---
+++ utils/usePWA/src/lib/components/Tooltip.tsx
@@ -0,0 +1,5 @@
+import './tooltip.css';
+
+export default function Tooltip() {
+  return <span className="tooltiptext">Install this webapp on your device. tap "share" and then Add to Homescreen</span>;
+}
