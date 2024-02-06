db02f2697598541f2113188d4a718a21b2ccac74
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-06 13:35:56 +0100

    test(mock-server): #19 wrote event bus instantiation and event subscription unit test

--- utils/event-bus/src/lib/event-bus.spec.ts
+++ utils/event-bus/src/lib/event-bus.spec.ts
@@ -1,7 +1,28 @@
-import { eventBus } from './event-bus';
+import { EventBus } from './event-bus';
 
 describe('eventBus', () => {
-  it('should work', () => {
-    expect(eventBus()).toEqual('event-bus');
+  const eventBus = EventBus.init();
+
+  it('should be instantiated', () => {
+    expect(eventBus).toBeDefined();
+    expect(eventBus).toEqual({ listeners: {} });
+  });
+
+  it('Should subscribe to events', () => {
+    // Subscriber 1
+    const callback1 = jest.fn((data) => {
+      console.log('Subscriber 1 received event1 with data:', data);
+    });
+    eventBus.subscribe('event1', callback1);
+
+    // Subscriber 2
+    const callback2 = jest.fn((data) => {
+      console.log('Subscriber 2 received event2 with data:', data);
+    });
+    eventBus.subscribe('event2', callback2);
+
+    expect(eventBus).toEqual({
+      listeners: { event1: [callback1], event2: [callback2] },
+    });
   });
 });
