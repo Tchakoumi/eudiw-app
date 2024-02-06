5ee21a51f67c064c6daaff669dfc798ba1a33382
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-06 13:33:27 +0100

    feat(mock-server): #19 implemented a minimum event bus service class

--- utils/event-bus/src/lib/event-bus.ts
+++ utils/event-bus/src/lib/event-bus.ts
@@ -1,3 +1,34 @@
-export function eventBus(): string {
-  return 'event-bus';
+interface EventListener {
+  (data: unknown): void;
+}
+
+export class EventBus {
+  private static instance: EventBus;
+  private listeners: { [event: string]: EventListener[] };
+
+  private constructor() {
+    this.listeners = {};
+  }
+
+  static init(): EventBus {
+    if (!EventBus.instance) {
+      EventBus.instance = new EventBus();
+    }
+    return EventBus.instance;
+  }
+
+  subscribe(event: string, callback: EventListener): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  publish(event: string, data: unknown): void {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach(callback => {
+        callback(data);
+      });
+    }
+  }
 }
