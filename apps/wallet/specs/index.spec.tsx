ec234517d3fc23ad9fa6bf39b67da10052022da9
Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-06 14:05:19 +0100

    test(shared): #2 mocked the window.matchMedia method for tests

--- apps/wallet/specs/index.spec.tsx
+++ apps/wallet/specs/index.spec.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-
 import Index from '../pages/index';
-
 jest.mock('next/router', () => require('next-router-mock'));
 
+// Mock window.matchMedia
 describe('Index', () => {
+  let matchMediaMock: jest.Mock;
+
+  beforeEach(() => {
+    matchMediaMock = jest.fn();
+    window.matchMedia = matchMediaMock;
+  });
+
+  afterEach(() => {
+    matchMediaMock.mockClear();
+  });
+
   it('should render successfully', () => {
+    // Mock the return value of window.matchMedia
+    matchMediaMock.mockReturnValue({
+      matches: true,
+      addListener: jest.fn(),
+    });
+
     const { baseElement } = render(<Index />);
     expect(baseElement).toBeTruthy();
   });
