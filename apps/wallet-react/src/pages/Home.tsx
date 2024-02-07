9e78886c0e0ec82db331551fd81cef0744888d9b
Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-07 17:48:06 +0100

    refactor(app): #18 instantiate usePWA in a more global way for all components

--- apps/wallet-react/src/pages/Home.tsx
+++ apps/wallet-react/src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import { usePWA } from '@datev/usePWA';
 import { Box } from '@mui/material';
 import Footer from '../components/home/Footer';
 import Header from '../components/home/Header';
 import HomeBody from '../components/home/HomeBody';
 
 export default function Home() {
-  usePWA();
-
   return (
     <Box
       sx={{
