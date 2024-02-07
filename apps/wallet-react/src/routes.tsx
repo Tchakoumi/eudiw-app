8a8d8dec889c1aa97ea068cc8d7cf1d672b00ef6
gis-ws02 <gis-ws02@adorsys.com>
2024-02-07 14:30:43 +0100

    chore(app): #18 configured credential page in routes

--- apps/wallet-react/src/routes.tsx
+++ apps/wallet-react/src/routes.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Scan from './pages/scan/Scan';
+import SelectCredential from './pages/selectCredential/SelectCredential';
 
 export interface IRoute {
   path: string;
@@ -16,6 +17,10 @@ export const routes: IRoute[] = [
   {
     element: <Scan />,
     path: '/scan',
+  },
+  {
+    element: <SelectCredential />,
+    path: '/credential-type',
   },
   {
     element: <Navigate to="/" />,
