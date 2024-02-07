d3cf270a316c0b5ac85c687107a6ec07824bf32e
gis-ws02 <gis-ws02@adorsys.com>
2024-02-07 14:25:33 +0100

    chore(app): #18 testing credential selection page

---
+++ apps/wallet-react/src/pages/selectCredential/SelectCredential.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function SelectCredential() {
+  return (
+    <Box>
+        <Box style={{padding:'4px 2px', backgroundColor: 'white', color:'black', borderRadius: '2px', border:'1px solid grey'}}>
+            This is the first credential
+        </Box>
+        <Box style={{padding:'4px 2px', backgroundColor: 'white', color:'black', borderRadius: '2px', border:'1px solid grey'}}>
+            This is the second credential
+        </Box>
+    </Box>
+  )
+}
