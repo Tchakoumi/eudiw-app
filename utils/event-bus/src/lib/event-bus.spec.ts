1a173321516e21a7ff93fe90141d4ec9a9bff373
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-09 11:40:39 +0100

    feat(event-bus): #19 Provided a wrapper class over EventEmitter class providing a central communication channel for emitting and listening to events

--- utils/event-bus/src/lib/event-bus.spec.ts
+++ utils/event-bus/src/lib/event-bus.spec.ts
@@ -1,4 +1,5 @@
-import { EventBus } from './event-bus';
+import EventEmitter from 'eventemitter3';
+import eventBus from './event-bus';
 
 enum Events {
   Event1 = 'event1',
@@ -6,44 +7,45 @@ enum Events {
 }
 
 describe('eventBus', () => {
-  let eventBus: EventBus, callback1: jest.Func, callback2: jest.Func;
+  let callback1: jest.Func, callback2: jest.Func;
 
   beforeAll(() => {
-    eventBus = EventBus.init();
     callback1 = jest.fn((data) => {
       console.log('Subscriber 1 received event1 with data:', data);
     });
-    callback2 = jest.fn((data) => {
-      console.log('Subscriber 2 received event2 with data:', data);
+    callback2 = jest.fn(async (data) => {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          console.time(`Subscriber 2 received event2 with ${data}`);
+          resolve(1);
+        }, 5000);
+      });
     });
   });
 
   it('should be instantiated', () => {
     expect(eventBus).toBeDefined();
-    expect(eventBus).toBeInstanceOf(EventBus);
-    expect(eventBus).toEqual({ listeners: {} });
+    expect(eventBus).toBeInstanceOf(EventEmitter);
   });
 
   it('Should subscribe to events', () => {
     // Subscriber 1
-    eventBus.subscribe(Events.Event1, callback1);
+    eventBus.on(Events.Event1, callback1);
 
     // Subscriber 2
-    eventBus.subscribe(Events.Event2, callback2);
+    eventBus.on(Events.Event2, callback2);
 
-    expect(eventBus).toEqual({
-      listeners: { [Events.Event1]: [callback1], [Events.Event2]: [callback2] },
-    });
+    expect(eventBus.eventNames()).toEqual([Events.Event1, Events.Event2]);
   });
 
   it('Should publish events', () => {
     const data1 = { message: 'Hello from publisher!' };
-    eventBus.publish(Events.Event1, data1);
+    eventBus.emit(Events.Event1, data1);
     expect(callback1).toHaveBeenCalled();
     expect(callback1).toHaveBeenCalledWith(data1);
 
     const data2 = { message: 'Hello again from publisher!' };
-    eventBus.publish(Events.Event2, data2);
+    eventBus.emit(Events.Event2, data2);
     expect(callback2).toHaveBeenCalled();
     expect(callback2).toHaveBeenCalledWith(data2);
   });
--- utils/event-bus/src/lib/event-bus.ts
+++ utils/event-bus/src/lib/event-bus.ts
@@ -1,34 +1,31 @@
-interface EventListener {
-  (data: unknown): void;
-}
+import { EventEmitter } from 'eventemitter3';
 
-export class EventBus {
-  private static instance: EventBus;
-  private listeners: { [event: string]: EventListener[] };
+/**
+ * EventBus class represents a singleton event bus instance using EventEmitter3.
+ * It provides a central communication channel for emitting and listening to events.
+ */
+class EventBus {
+  /**
+   * Singleton instance of the EventEmitter.
+   * It holds the reference to the unique event bus instance.
+   */
+  private static instance: EventEmitter;
 
-  private constructor() {
-    this.listeners = {};
-  }
-
-  static init(): EventBus {
+  /**
+   * Initializes the event bus instance.
+   * If the instance doesn't exist, creates a new one; otherwise, returns the existing instance.
+   * @returns The initialized EventEmitter instance representing the event bus.
+   */
+  static init(): EventEmitter {
+    // Check if the instance already exists
     if (!EventBus.instance) {
-      EventBus.instance = new EventBus();
+      // If the instance doesn't exist, create a new EventEmitter instance
+      EventBus.instance = new EventEmitter();
     }
+    // Return the initialized instance
     return EventBus.instance;
   }
-
-  subscribe(event: string, callback: EventListener): void {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
-    }
-    this.listeners[event].push(callback);
-  }
-
-  publish(event: string, data: unknown): void {
-    if (this.listeners[event]) {
-      this.listeners[event].forEach(callback => {
-        callback(data);
-      });
-    }
-  }
 }
+
+// Initialize and export the singleton instance of the event bus
+export default EventBus.init();
