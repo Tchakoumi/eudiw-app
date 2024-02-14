cd37032f3bd335b9c87784253c0651d4bb3e984a
Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-14 16:54:06 +0100

    fix(app): #18 added settings to header, centeralised text
    added settings to header and centralised text, also made text as props for reusability based on credentials page

--- apps/wallet-react/src/components/home/Header.tsx
+++ apps/wallet-react/src/components/home/Header.tsx
@@ -1,16 +1,30 @@
-import { Box, Typography } from '@mui/material';
+import settings from '@iconify/icons-fluent/settings-24-regular';
+import { Icon } from '@iconify/react';
+import { Box, IconButton, Tooltip, Typography } from '@mui/material';
 
-export default function Header() {
+export default function Header({
+  pageTitle = 'DATEV Wallet',
+}: {
+  pageTitle?: string;
+}) {
   return (
     <Box
       sx={{
-        padding: '0 16px',
+        padding: '12px',
         display: 'grid',
-        gap: 1.2,
-        marginBottom: 2,
+        gridTemplateColumns: '1fr auto',
+        justifyItems: 'center',
+        alignItems: 'center',
       }}
     >
-      <Typography variant="h2">DATEV-Wallet</Typography>
+      <Typography sx={{ fontWeight: 600, fontSize: '29px' }}>
+        {pageTitle}
+      </Typography>
+      <Tooltip arrow title="Settings">
+        <IconButton size="small">
+          <Icon icon={settings} fontSize={24} />
+        </IconButton>
+      </Tooltip>
     </Box>
   );
 }
