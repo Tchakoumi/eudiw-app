7bf15d60e3526eee0664dc3d0b8356a80f8289e8
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-16 17:35:07 +0100

    test(storage): #24 writing more test

--- utils/storage/src/lib/Storage.spec.ts
+++ utils/storage/src/lib/Storage.spec.ts
@@ -2,21 +2,40 @@ import { DBSchema } from 'idb';
 import { StorageFactory } from './StorageFactory';
 
 // Mocking indexdedDB functionality
-import "core-js/stable/structured-clone";
-import "fake-indexeddb/auto";
+import 'core-js/stable/structured-clone';
+import 'fake-indexeddb/auto';
 
 interface TestDBSchema extends DBSchema {
-  test_store: {
+  testStore: {
     key: string;
     value: string;
+  };
+  inlineKeyStore: {
+    key: string;
+    value: {
+      id: string;
+      name: string;
+      email: string;
+    };
   };
 }
 
 describe('StorageFactory', () => {
-  const storageFactory = new StorageFactory<TestDBSchema>('test_db', 1, {
-    upgrade(db) {
-      db.createObjectStore('test_store');
-    },
+  let storageFactory: StorageFactory<TestDBSchema>;
+
+  const payload1 = { key: 'test_key', value: 'test_value' };
+  const payload2 = {
+    key: 'test_in_line_key',
+    value: { id: 'test_in_line_key', name: 'Joe', email: 'joe237@gmail.com' },
+  };
+
+  beforeAll(() => {
+    storageFactory = new StorageFactory<TestDBSchema>('testDB', 1, {
+      upgrade(db) {
+        db.createObjectStore('testStore');
+        db.createObjectStore('inlineKeyStore', { keyPath: 'id' });
+      },
+    });
   });
 
   it('should be defined', async () => {
@@ -24,10 +43,15 @@ describe('StorageFactory', () => {
   });
 
   it('should insert data', async () => {
-    const addedKey = await storageFactory.insert('test_store', {
-      key: 'test_key',
-      value: 'test_value',
-    });
-    expect(addedKey).toBe('test_key');
+    const addedKey1 = await storageFactory.insert('testStore', payload1);
+    expect(addedKey1).toBe('test_key');
+
+    const addedKey2 = await storageFactory.insert('inlineKeyStore', payload2);
+    expect(addedKey2).toEqual('test_in_line_key');
+  });
+
+  it('should find one', async () => {
+    const record = await storageFactory.findOne('testStore', 'test_key');
+    expect(record).toEqual(payload1);
   });
 });
