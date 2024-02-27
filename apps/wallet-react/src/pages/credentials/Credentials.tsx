9ef5bf78e363d16dfc4471ea8e26e144c41f058b
Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-27 11:03:27 +0100

    fix(app): #33 adjusted credentials overflow distorting layout view
    added scrollbars to hold, adjusted padding

--- apps/wallet-react/src/pages/credentials/Credentials.tsx
+++ apps/wallet-react/src/pages/credentials/Credentials.tsx
@@ -1,4 +1,5 @@
 import { Box, Snackbar } from '@mui/material';
+import Scrollbars from 'rc-scrollbars';
 import { useEffect, useState } from 'react';
 import AuthleteLogo from '../../assets/authlete-logo.png';
 import { IVerifiableCredential } from '../../components/credential-offer/credentials.types';
@@ -72,33 +73,35 @@ export default function Credentials() {
         }}
       >
         <Header />
-        <Box
-          sx={{
-            display: 'grid',
-            rowGap: 1,
-            padding: '0 16px',
-            alignContent: 'start',
-            backgroundColor: '#F6F7F9',
-            paddingTop: 3,
-          }}
-        >
-          {isLoadingCredentials ? (
-            [...new Array(4)].map((_, index) => (
-              <CredentialCardSkeleton key={index} />
-            ))
-          ) : credentials.length === 0 ? (
-            <NoCredentials />
-          ) : (
-            <Box>
-              {credentials.map((credential, index) => (
-                <CredentialCard
-                  credential={credential}
-                  key={index}
-                  openDetails={() => setSelectedCredential(credential)}
-                />
-              ))}
+        <Box sx={{ height: '100%', backgroundColor: '#F6F7F9' }}>
+          <Scrollbars autoHide universal>
+            <Box
+              sx={{
+                display: 'grid',
+                rowGap: 1,
+                padding: '12px',
+                alignContent: 'start',
+              }}
+            >
+              {isLoadingCredentials ? (
+                [...new Array(4)].map((_, index) => (
+                  <CredentialCardSkeleton key={index} />
+                ))
+              ) : credentials.length === 0 ? (
+                <NoCredentials />
+              ) : (
+                <Box>
+                  {credentials.map((credential, index) => (
+                    <CredentialCard
+                      credential={credential}
+                      key={index}
+                      openDetails={() => setSelectedCredential(credential)}
+                    />
+                  ))}
+                </Box>
+              )}
             </Box>
-          )}
+          </Scrollbars>
         </Box>
 
         <Footer showArrow={false} />
