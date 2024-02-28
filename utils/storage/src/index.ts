70b4c134ccd692694531dbc1b5ee08bcf2e38f4b
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-28 15:01:39 +0100

    chore(storage): #24 adjusted storage type

--- utils/storage/src/index.ts
+++ utils/storage/src/index.ts
@@ -1,3 +1,13 @@
-export type * from './lib/Storage.types';
+export {
+  MethodNames,
+  QueryStore,
+  StorageMethodType,
+  StorageTransaction,
+  StoreIndexNames,
+  StoreRecord,
+  StoreRecordKey,
+  StoreRecordValue,
+  TransactionCallback,
+} from './lib/Storage.types';
 export { StorageFactory } from './lib/StorageFactory';
 export { ApplyClassWrapper } from './lib/error/ApplyClassWrapper';
