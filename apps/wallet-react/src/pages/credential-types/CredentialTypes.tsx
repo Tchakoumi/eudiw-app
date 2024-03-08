d343560398a535b703eeda28019120d854c3b430
Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-08 10:26:21 +0100

    feat(app): #38 extracted crendential utility functiosn to external ts file

--- apps/wallet-react/src/pages/credential-types/CredentialTypes.tsx
+++ apps/wallet-react/src/pages/credential-types/CredentialTypes.tsx
@@ -7,16 +7,14 @@ import { useNavigate } from 'react-router-dom';
 import CredentialTypeCard from '../../components/credential-types/CredentialTypeCard';
 import CredentialTypeDetails from '../../components/credential-types/CredentialTypeDetails';
 import {
-  CredentialIssuerMetadata,
   CredentialOfferResponse,
   CredentialOfferResponsePayload,
   ICredentialCard,
   ISupportedCredential,
-  VCSDJWTClaim,
 } from '../../components/credential-types/credentials.types';
 import BackTitleBar from '../../components/layout/BackTitleBar';
 import Footer from '../../components/layout/Footer';
-import { removeUnderscoresFromWord } from '../../utils/common';
+import { getVCClaims, getVCSDJWTOffers } from './credentialType.util';
 
 export default function CredentialTypes() {
   const push = useNavigate();
@@ -32,103 +30,6 @@ export default function CredentialTypes() {
       }
     );
   }, []);
-
-  /**
-   * This function helps to get the selected credential type's
-   * claims in the prefered locale.
-   *
-   * If the provided preferred locale is found, it'll return it's value
-   * else it'll use the value of the the first element of display
-   * if the claim has no display, then it'll remove underscores from
-   * the claim and return as value for display
-   *
-   * @param {VCSDJWTClaim} claims - the different claims present in the credential type
-   * @param {string} preferredLocale - the desired language in which we want the claims to be presented
-   * @returns {string[]} - a list of all claims in preferred locale
-   */
-  function getVCClaimsInPreferredLocale(
-    claims: VCSDJWTClaim,
-    preferredLocale: string
-  ): string[] {
-    const claimKeys = Object.keys(claims) as (keyof typeof claims)[];
-
-    const claimKeysInPreferredLocal = claimKeys.map((claimKey) => {
-      if (claims[claimKey].display.length > 0) {
-        const claimInPreferredLocale = claims[claimKey].display.find(
-          ({ locale }) => locale === preferredLocale
-        );
-        // if preferred locale is found, then retun the name in that locale
-        if (claimInPreferredLocale) return claimInPreferredLocale.name;
-        //if preferred local is not found, just return the name on first element in display list
-        return claims[claimKey].display[0].name;
-      }
-      // if the display list is empty return the cleanedup key
-      return removeUnderscoresFromWord(claimKey as string);
-    });
-    return claimKeysInPreferredLocal;
-  }
-
-  /**
-   * Gets all credential types of vc+sd-jwt format, adding to it the issuer and it's credential type
-   *
-   * @param {CredentialIssuerMetadata} issuer_metadata - the provided metadata from issuer
-   * @returns {ICredentialCard} - the metadata credential configurations supported, type and issuer of every supported type
-   */
-  function getVCSDJWTOffers(
-    issuer_metadata: CredentialIssuerMetadata
-  ): ICredentialCard[] {
-    const credentialOfferTypeKeys = Object.keys(
-      issuer_metadata.credential_configurations_supported
-    ) as ISupportedCredential[];
-
-    const vcSdJwtTypeKeys = credentialOfferTypeKeys.filter(
-      (credentialType) =>
-        issuer_metadata.credential_configurations_supported[credentialType]
-          .format === 'vc+sd-jwt'
-    );
-    return vcSdJwtTypeKeys.map((credentialOfferTypeKey) => {
-      return {
-        type: credentialOfferTypeKey,
-        issuer: issuer_metadata.credential_issuer,
-        data: issuer_metadata.credential_configurations_supported[
-          credentialOfferTypeKey
-        ],
-      };
-    }) as ICredentialCard[];
-  }
-
-  /**
-   * Serves as guard to get the claims in preferred locale.
-   * verifies that provided selectedCredential exists, then moves on to get claims in provided locale.
-   * if it doesn't exist, it returns and empty array
-   *
-   * @param {ISupportedCredential} selectedCredential - the credential type's key who's claims we want
-   * @param {typeof ResolvedCredentialOffer} credentialIssuerMetadata - the provided metadata from issuer
-   * @param {string} preferredLocale - the desired language in which we want the claims to be presented
-   * @returns {string[]} - the list of claims to display
-   */
-  function getVCClaims(
-    selectedCredential: ISupportedCredential,
-    credentialIssuerMetadata: CredentialIssuerMetadata,
-    preferredLocale: string
-  ): string[] {
-    const offeredCredentialTypeKeys = Object.keys(
-      credentialIssuerMetadata.credential_configurations_supported
-    ) as ISupportedCredential[];
-
-    if (offeredCredentialTypeKeys.includes(selectedCredential)) {
-      const selectedOfferClaims =
-        credentialIssuerMetadata.credential_configurations_supported[
-          selectedCredential
-        ].claims;
-
-      return getVCClaimsInPreferredLocale(
-        selectedOfferClaims as VCSDJWTClaim,
-        preferredLocale
-      );
-    }
-    return [];
-  }
 
   const [selectedCredentialType, setSelectedCredentialType] =
     useState<ICredentialCard>();
