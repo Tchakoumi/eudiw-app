466d7366517dfde884b4a83f8f3de96f8ffb0d79
Ingrid Kamga <ingrid.kamga@clemios.com>
2024-03-12 09:02:11 +0100

    fix(proxy-server): propagate query string

--- apps/proxy-server/src/main.ts
+++ apps/proxy-server/src/main.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // Listen on a specific port via the PORT environment variable
@@ -9,22 +9,45 @@ const port = process.env.PROXY_SERVER_PORT
 // Create an Express application
 const app = express();
 
-// Proxy all requests to URL-assumed path
-app.use(
-  createProxyMiddleware({
-    router: (req) => new URL(req.path.substring(1)),
-    pathRewrite: (path) => new URL(path.substring(1)).pathname,
+// Proxy all requests
+app.use((req, res, next) => {
+  // The new target is assumed to be the full path
+  // of the current request.
+  let target: URL;
+
+  try {
+    target = new URL(req.originalUrl.substring(1));
+  } catch (e) {
+    // The new target must be a compliant URL
+    res.status(400).send('Invalid URL');
+    return;
+  }
+
+  const proxy = createProxyMiddleware({
+    // Re-target request
     changeOrigin: true,
+    target: target.origin,
+    pathRewrite: () => target.href.substring(target.origin.length),
+
+    // Enable Cross-Origin Resource Sharing (CORS)
     onProxyRes: (proxyRes) => {
-      // Enable Cross-Origin Resource Sharing (CORS)
-      proxyRes.headers['access-control-allow-origin'] = '*';
-      proxyRes.headers['access-control-allow-headers'] = '*';
+      proxyRes.headers['access-control-allow-origin'] = '';
+      proxyRes.headers['access-control-allow-headers'] = '';
       delete proxyRes.headers['set-cookie'];
     },
-  })
-);
+  });
+
+  // Proxy the request
+  proxy(req, res, next);
+});
+
+// Lazy error handler
+app.use((err: Error, req: Request, res: Response) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
 
 // Start the Express server on the specified port
 app.listen(port, () => {
-  console.log(`proxy server is running on port ${port}`);
+  console.log(`[proxy-server] running on port ${port}`);
 });
