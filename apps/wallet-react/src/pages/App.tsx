f6e6e01500e74410afdcd3113d3cea2ad51016cd
Ingrid Kamga <ingrid.kamga@clemios.com>
2024-03-13 11:13:59 +0100

    fix(oid4vci): revert changes to frontend code

--- apps/wallet-react/src/pages/App.tsx
+++ apps/wallet-react/src/pages/App.tsx
@@ -4,16 +4,8 @@ import '../assets/styles/global.css';
 import { routes } from '../routes';
 import { useTheme } from '../utils/theme';
 import { usePWA } from '@datev/usePWA';
-import { OID4VCIServiceImpl } from '@datev/oid4vci';
-import { eventBus } from '@datev/event-bus';
-import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    const service = new OID4VCIServiceImpl(eventBus);
-    console.log({ env: process.env });
-  });
-
   usePWA();
   const routing = useRoutes(routes);
   return <ThemeProvider theme={useTheme()}>{routing}</ThemeProvider>;
