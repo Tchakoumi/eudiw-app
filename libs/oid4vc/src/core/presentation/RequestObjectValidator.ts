1b3c3d0c0863761d58e2ddd97aa32be9e006e653
Marco Kuidja <kuidjamarco@gmail.com>
2024-03-20 13:49:30 +0100

    feat(oid4vc): #48 added support for `x509_san_dns` & `redirect_uri` client id scheme

---
+++ libs/oid4vc/src/core/presentation/RequestObjectValidator.ts
@@ -0,0 +1,112 @@
+import * as jose from 'jose';
+import { OID4VCIServiceError } from '../../lib/errors';
+import { PresentationError } from '../../lib/errors/Presentation.errors';
+import { ClientIdScheme, RequestObject } from '../../lib/types';
+import { isValidDNSName } from '../../utils';
+
+export class RequestObjectValidator {
+  /**
+   * Constructor.
+   */
+  public constructor() {}
+
+  /**
+   * Validate an authorization request object matching the specified client identifier scheme
+   *  @see https://openid.net/specs/openid-4-verifiable-presentations-1_0-20.html#name-verifier-metadata-managemen
+   *
+   * @param requestObjectJwt JWT Signed request object
+   * @returns
+   */
+  validate(requestObjectJwt: string) {
+    const requestObject = this.decodeRequestJwt(requestObjectJwt);
+    if (!requestObject.redirect_uri && !requestObject.response_uri) {
+      throw new OID4VCIServiceError(PresentationError.MissingResponseParams);
+    }
+
+    switch (requestObject.client_id_scheme) {
+      case ClientIdScheme.X509_SAN_DNS:
+        return this.x509SanDnsSchemeValidator(requestObjectJwt);
+      case ClientIdScheme.REDIRECT_URI:
+        // request must not be signed when `client_id_scheme` is set to `redirect_uri`
+        //https://openid.net/specs/openid-4-verifiable-presentations-1_0-20.html#section-5.7-3.2.1
+        throw new OID4VCIServiceError(PresentationError.MisusedClientIdScheme);
+
+      default:
+        throw new OID4VCIServiceError(
+          PresentationError.InvalidRequestObjectJwt
+        );
+    }
+  }
+
+  redirectUriSchemeValidator(requestObject: RequestObject) {
+    if (requestObject.client_id !== requestObject.redirect_uri) {
+      throw new OID4VCIServiceError(PresentationError.MismatchedClientId);
+    }
+
+    return requestObject;
+  }
+
+  async x509SanDnsSchemeValidator(requestObjectJwt: string) {
+    let header: jose.ProtectedHeaderParameters;
+    try {
+      header = jose.decodeProtectedHeader(requestObjectJwt);
+    } catch (e) {
+      throw new OID4VCIServiceError(PresentationError.InvalidRequestObjectJwt);
+    }
+
+    if (!header?.x5c || !header?.kid)
+      throw new OID4VCIServiceError(
+        PresentationError.MissingJwtRequiredHeaderParams
+      );
+    const jwk = await this.createJWKFromX5C(header.x5c);
+
+    let requestObject: RequestObject;
+    try {
+      const result = await jose.jwtVerify<RequestObject>(requestObjectJwt, jwk);
+      requestObject = result.payload;
+    } catch (error) {
+      throw new OID4VCIServiceError(
+        PresentationError.InvalidRequestObjectJwtSignature
+      );
+    }
+
+    if (!requestObject.client_id || !isValidDNSName(requestObject.client_id)) {
+      throw new OID4VCIServiceError(PresentationError.MismatchedClientId);
+    }
+
+    return requestObject;
+  }
+
+  /**
+   * Decodes a request object jwt string
+   * @param requestObjectJwt Request object encryted in jwt
+   * @returns
+   */
+  private decodeRequestJwt(requestObjectJwt: string) {
+    try {
+      return jose.decodeJwt<RequestObject>(requestObjectJwt);
+    } catch (e) {
+      throw new OID4VCIServiceError(PresentationError.InvalidRequestObjectJwt);
+    }
+  }
+
+  /**
+   * Create a JWK (JSON Web Key) from the X.509 certificate chain
+   * @param x5cArray `x5c` from jwt header
+   */
+  private async createJWKFromX5C(x5cArray: string[]) {
+    // Convert x5c to PEM format
+    const pemCert = `-----BEGIN CERTIFICATE-----\n${x5cArray.join(
+      '\n'
+    )}\n-----END CERTIFICATE-----`;
+
+    try {
+      // Create JWK from X.509 certificate chain
+      return await jose.importX509(pemCert, 'pem');
+    } catch (error) {
+      throw new OID4VCIServiceError(
+        PresentationError.UnResolvedJwKHeaderParams
+      );
+    }
+  }
+}
