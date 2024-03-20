ab94a0d8905898c816cec2b018d4c9789881aab2
Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-20 11:40:03 +0100

    refactor(app): #69 extracted domain transformation to utility
    Closes: #69

--- apps/wallet-react/src/utils/common.ts
+++ apps/wallet-react/src/utils/common.ts
@@ -10,6 +10,19 @@ export function capitalize(phrase: string): string {
 }
 
 /**
+ * This functions takes a phrase and returns every word capitalized
+ *
+ * @param {strubg} phrase - phrase whoses words need be capitalized
+ * @returns {string} - the phrase with all words starting with capital letters
+ */
+export function capitalizeEveryWord(phrase: string): string {
+  return phrase
+    .split(' ')
+    .map((_) => capitalize(_))
+    .join(' ');
+}
+
+/**
  * This function removes underscores(_) in a word
  * and seperate in different words, all capitalized.
  *
@@ -21,4 +34,16 @@ export function removeUnderscoresFromWord(word: string): string {
     .split('_')
     .map((jj) => capitalize(jj))
     .join(' ');
+}
+
+/**
+ * This function is used to remove the dots (.)
+ * from a domain and return clear text without the tld
+ *
+ * @param domain - the domain that needs transformation
+ * @returns {string} - the string gotten from domain
+ */
+export function domainToClearString(domain: string) {
+  const splitedDomain = domain.split('.');
+  return splitedDomain.slice(0, splitedDomain.length - 1).join(' ');
 }
